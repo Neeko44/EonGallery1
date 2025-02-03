9ff8c6ed2397e1257175a02d92a2bfe2b4150626
unknown <achmadroyhanun671@gmail.com>
2025-02-03 11:52:13 +0700

    uslukusluk

---
+++ database/migrations/2025_02_03_035142_add_foreign_keys_to_order_items_table.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        // Foreign key untuk tabel users
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign('ID_MUSEUM')->references('ID_MUSEUM')->on('museum');
+        });
+
+        // Foreign key untuk tabel artworks
+        Schema::table('artworks', function (Blueprint $table) {
+            $table->foreign('ID_KATEGORI')->references('ID_KATEGORI')->on('kategori');
+            $table->foreign('ID_MUSEUM')->references('ID_MUSEUM')->on('museum');
+        });
+
+        // Foreign key untuk tabel order_items
+        Schema::table('order_items', function (Blueprint $table) {
+            $table->foreign('ID_PEMBAYARAN')->references('ID_PEMBAYARAN')->on('pembayaran');
+            $table->foreign('ID_TICKET')->references('ID_TICKET')->on('tickets');
+        });
+
+        // Foreign key untuk tabel pembayaran
+        Schema::table('pembayaran', function (Blueprint $table) {
+            $table->foreign('ID_ORDER')->references('ID_ORDER')->on('order_items');
+        });
+
+        // Foreign key untuk tabel tickets
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->foreign('ID_USER')->references('ID_USER')->on('users');
+            $table->foreign('ID_ORDER')->references('ID_ORDER')->on('order_items');
+            $table->foreign('ID_MUSEUM')->references('ID_MUSEUM')->on('museum');
+        });
+
+    }
+
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['ID_MUSEUM']);
+        });
+
+        Schema::table('artworks', function (Blueprint $table) {
+            $table->dropForeign(['ID_KATEGORI']);
+            $table->dropForeign(['ID_MUSEUM']);
+        });
+
+        Schema::table('order_items', function (Blueprint $table) {
+            $table->dropForeign(['ID_PEMBAYARAN']);
+            $table->dropForeign(['ID_TICKET']);
+        });
+
+        Schema::table('pembayaran', function (Blueprint $table) {
+            $table->dropForeign(['ID_ORDER']);
+        });
+
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->dropForeign(['ID_USER']);
+            $table->dropForeign(['ID_ORDER']);
+            $table->dropForeign(['ID_MUSEUM']);
+        });
+
+    }
+};
