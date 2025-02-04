cfa465eeebe47fa245e4c22b3c27ee1994bce6ff
Nobberu <itsnobberu@gmail.com>
2025-02-04 11:12:05 +0700

    Migration fix & Env Default Changes

--- database/migrations/2025_02_03_032601_create_users_table.php
+++ database/migrations/2025_02_03_032601_create_users_table.php
@@ -4,12 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('ID_USER');
+            $table->unsignedInteger('ID_MUSEUM');
             $table->string('NAMA_USER', 30);
             $table->string('FOTO_USER', 255)->nullable();
             $table->string('EMAIL', 100)->unique();
